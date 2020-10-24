ad61b54ec8da55edefd46cd6984bf101359e6416
kzagorulko <kostrock@gmail.com>
2020-10-25 01:04:16 +0300

    init backend, added readmes, added migrations

---
+++ backend/flower/resources/__init__.py
@@ -0,0 +1,13 @@
+from starlette.routing import Route, Mount
+from starlette.responses import JSONResponse
+
+from . import users
+
+
+async def ping(request):
+    return JSONResponse({'onPing': 'wePong'})
+
+routes = [
+    Route('/ping', ping),
+    Mount('/users', routes=users.routes),
+]
