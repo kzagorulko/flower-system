c8853c46a974f7037b543ae671a24676a2cc6b29
DeuteriumQ <phil.deutrm@gmail.com>
2020-10-27 21:01:16 +0300

    React-Admin integration. dataProvider v0.0.1 (GET,POST,PATCH). UserResource patch() fix. CORS

--- backend/flower/application.py
+++ backend/flower/application.py
@@ -1,13 +1,24 @@
 from starlette.applications import Starlette
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
 
 from .resources import routes
+
+middleware = [
+    Middleware(
+        CORSMiddleware,
+        allow_origins=['*'],
+        allow_methods=['POST', 'GET', 'PATCH', 'DELETE'],
+        allow_headers=['content-type'])
+]
 
 
 def create_app():
     from .models import db
     app = Starlette(
         debug=True,
-        routes=routes
+        routes=routes,
+        middleware=middleware
     )
 
     db.init_app(app)
