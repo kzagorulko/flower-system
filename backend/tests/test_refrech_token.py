01a2f71153427905a4351c01535400ef5f1d98d3
Ivan Bugaev <ivan-753s@mail.ru>
2020-10-31 18:48:15 +0300

    #2 Tests backend setup

---
+++ backend/tests/test_refrech_token.py
@@ -0,0 +1,9 @@
+# import packages as
+# import jwt
+
+# import params from packages as
+# from ..flower.resources.users import create_refresh_token
+
+
+def test_create_refresh_token_success():
+    assert True
