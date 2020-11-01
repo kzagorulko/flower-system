b671e85d6fb8b9d20d492708645caafe5d989be5
Ivan Bugaev <ivan-753s@mail.ru>
2020-11-01 12:55:44 +0300

    Remove useless comments

--- backend/tests/test_refrech_token.py
+++ backend/tests/test_refrech_token.py
@@ -1,9 +1,2 @@
-# import packages as
-# import jwt
-
-# import params from packages as
-# from ..flower.resources.users import create_refresh_token
-
-
 def test_create_refresh_token_success():
     assert True
