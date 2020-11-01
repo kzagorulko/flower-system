0e7cdd1177c718db58db54be82713090c52d75db
Ph <31614511+DeuteriumQ@users.noreply.github.com>
2020-11-01 12:26:38 +0300

    #15 Feature/front auth (#17)
    * Added authProvider login function. Removed 'identity' field from user post.
    
    * Reworked and finalyzed authProvider. Added cookies. Reworked axios calls. Few backend fixes
    
    * Replaced missed localStorage with cookie management
    
    * Edit (update) user Unhandled promise fix. Notification example. Refresh-token expiry handling
    
    Co-authored-by: Konstantin Zagorulko <31614542+kzagorulko@users.noreply.github.com>
    
    closes #15

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Admin, Resource } from 'react-admin';
 import dataProvider from './api/dataProvider';
+import authProvider from './api/authProvider';
 import { UserList, UserCreate, UserEdit } from './components/UserList';
 import UserShow from './components/UserShow';
 
 import './App.less';
 
 const App = () => (
-  <Admin title="Example" dataProvider={dataProvider('http://127.0.0.1:8000')}>
+  <Admin title="Example" dataProvider={dataProvider} authProvider={authProvider}>
     <Resource name="users" list={UserList} show={UserShow} create={UserCreate} edit={UserEdit} />
   </Admin>
 );
