ed3d463bf780d06d071ac956c962f2c718f1b982
mik0war <anton.korneev.2011@yandex.ru>
2020-11-08 00:11:49 +0300

    Create BranchModel and User_x_model

---
+++ backend/flower/core/branches/models.py
@@ -0,0 +1,26 @@
+from ..database import db
+
+
+class BranchModel(db.Model):
+    __tablename__ = 'branches'
+
+    id = db.Column(db.Integer, primary_key=True)
+    address = db.Column(db.String, unique=True, nullable=False)
+
+    def jsonify(self, for_card=False):
+        result = {
+            'id': self.id,
+            'address': self.address,
+        }
+
+        if for_card:
+            result['address'] = self.address
+
+        return result
+
+
+class UserBranchModel(db.Model):
+    __tablename__ = 'user_x_branch'
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    branch_id = db.Column(db.Integer, db.ForeignKey('branches.id'), nullable=False)
--- backend/flower/core/models.py
+++ backend/flower/core/models.py
@@ -1,5 +1,6 @@
 from .roles.models import RoleModel
 from .users.models import UserModel
 from .permissions.models import PermissionModel
+from .branches.models import BranchModel, UserBranchModel
 
-__all__ = ['UserModel', 'RoleModel', 'PermissionModel']
+__all__ = ['UserModel', 'RoleModel', 'PermissionModel', 'BranchModel', 'UserBranchModel']
